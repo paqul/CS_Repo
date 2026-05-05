affc234d104cc1c725034bccf6bdf661dc008cd1
paqul <paqul.timeless@gmail.com>
2026-05-05 11:50:45 +0200

    add few lines

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HellowWorld
 {
@@ -10,8 +6,10 @@ namespace HellowWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-            Console.ReadKey();
+            System.Console.WriteLine("Hello World\n");
+            System.Console.ReadKey();
+            System.Console.WriteLine("End of the program");
+            System.Console.ReadKey();
         }
     }
 }
