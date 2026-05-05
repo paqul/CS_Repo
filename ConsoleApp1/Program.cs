6ff0ce7407039ace0ac677f9f78034f0f7230768
paqul <paqul.timeless@gmail.com>
2026-05-05 11:32:18 +0200

    First commit on master

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HellowWorld
+{
+    class Porgram
+    { static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
